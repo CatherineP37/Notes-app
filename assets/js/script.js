400648fee61128badbf853560beb3ce4359a14a1
CatherineP37 <137174676+CatherineP37@users.noreply.github.com>
2026-05-09 14:45:44 +0100

    Add JavaScript variables for the add input container and edit input container

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,7 @@
 let notes = []
+const addInputContainer = document.querySelector('.add-input-container');
 const addInput = document.getElementById('add_input');
+const editInputContainer = document.querySelector('.edit-input-container');
 const editInput = document.getElementById('edit_input');
 const editButton = document.getElementById('edit_button');
+
