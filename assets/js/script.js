95d1fb98e8d823c4a8a2db48b081aceefa862ba5
CatherineP37 <137174676+CatherineP37@users.noreply.github.com>
2026-05-09 14:55:42 +0100

    Add addNote() function to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 let notes = []
+const displayAddInput = document.querySelector('.display-add-input');
 const addInputContainer = document.querySelector('.add-input-container');
 const addInput = document.getElementById('add_input');
 const editInputContainer = document.querySelector('.edit-input-container');
@@ -6,3 +7,15 @@ const editInput = document.getElementById('edit_input');
 const editButton = document.getElementById('edit_button');
 const notesContainer = document.getElementById('notes_container');
 
+function addNote() {
+  displayAddInput.style.display = 'none';
+  let addInputValue = addInput.value;
+  if(addInputValue) {
+    notes.push(addInputValue);
+    displayNotes();
+    addInput.value = '';
+    addInputContainer.style.display = 'none';
+    displayAddInput.style.display = 'block';
+  }  
+ 
+}
