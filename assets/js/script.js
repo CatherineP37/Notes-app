6111300249414dc26528f4abc744b868c1ae0003
CatherineP37 <137174676+CatherineP37@users.noreply.github.com>
2026-05-09 10:04:17 +0100

    Add JavaScript variable for edit button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
 let notes = []
 const addInput = document.getElementById('add_input');
 const editInput = document.getElementById('edit_input');
+const editButton = document.getElementById('edit_button');
